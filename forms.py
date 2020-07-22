38a094bafdb9bbc5c4cd57c4938e6607b2373589
Sarfraz Baig <sarfrazbaig55@gmail.com>
2020-07-22 17:15:49 +0530

    Initial Commit

---
+++ forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField , PasswordField , SubmitField
+from wtforms.validators import DataRequired, EqualTo ,ValidationError
+
+
+class LoginForm(FlaskForm):
+    username= StringField('Username', validators=[DataRequired()])
+    password= PasswordField('Password', validators=[DataRequired()])
+    submit= SubmitField('Login')
+
+
+class RegistrationForm(FlaskForm):
+    username= StringField('Username', validators=[DataRequired()])
+    password= PasswordField('Password', validators=[DataRequired()])
+    confirm_password= PasswordField('Confirm Password', validators=[DataRequired(),EqualTo('password')])
+    submit= SubmitField('Sign Up')
+
+class Answer(FlaskForm):
+    answer=StringField('Answer:', validators=[DataRequired()])
+    submit= SubmitField('Submit Answer')
